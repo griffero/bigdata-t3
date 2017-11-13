dbe09c6758857f6835dec09d4eaa52f398d12225
Cristobal Griffero <cgriffero@miuandes.cl>
2017-11-13 14:26:38 -0300

    gitignore updated. p1 pre-code finished

--- p1.py
+++ p1.py
@@ -3,50 +3,55 @@ from mrjob.step import MRStep
 from mrjob.protocol import JSONValueProtocol
 from mr3px.csvprotocol import CsvProtocol
 import time
+import csv
 
 DATA_TYPE = 0
 CATEGORIES = 1
-STARS = 1
+REVIEW_ID = 1
+STARS = 2
 
 class CountBusinessStarsPerCategory(MRJob):
     INPUT_PROTOCOL = JSONValueProtocol
-    OUTPUT_PROTOCOL = CsvProtocol
 
-    def map_business_id_with_review_or_categories(self, _, line):
+    def initial_map(self, _, line):
         data = line
         if data.get("user_id"):
             # business_id, ["review", review_id, stars]
-            yield data["business_id"], ["review", data["stars"]]
+            yield data["business_id"], ["review", data["review_id"], data["stars"]]
         elif data.get("business_id"):
             # business_id, ["business", categories]
             yield data["business_id"], ["business", data["categories"]]
 
-    def reduce_business_information(self, key, values):
+    def reducer_join(self, key, values):
         business_raw_information = list(values)
-        business_stars = []
+        business_reviews = []
         business_categories = ""
         for business_information in business_raw_information:
             if business_information[DATA_TYPE] == "business":
                 business_categories = business_information[CATEGORIES]
             elif business_information[DATA_TYPE] == "review":
-                business_stars.append(business_information[STARS])
-        for star in business_stars:
+                business_reviews.append([business_information[REVIEW_ID], business_information[STARS]])
+        for review in business_reviews:
             for category in business_categories:
-                yield (category, star)
+                review_id = review[0]
+                review_stars = review[1]
+                output = [review_id, category, review_stars]
+                writer.writerow(output)
+                yield ("out", output)
 
-    def reduce_by_categories(self, key, values):
-        yield "out", [key, list(values)]
 
     def steps(self):
         return [
             MRStep(
-                mapper=self.map_business_id_with_review_or_categories,
-                reducer=self.reduce_business_information
-                ),
-            MRStep(
-                reducer=self.reduce_by_categories
+                mapper=self.initial_map,
+                reducer=self.reducer_join
                 )
             ]
 
 if __name__ == '__main__':
+    header = ["review_id", "category", "stars"]
+    output_file  = open('test.csv', "wb")
+    writer = csv.writer(output_file)
+    writer.writerow(header)
     CountBusinessStarsPerCategory.run()
+    output_file.close()
